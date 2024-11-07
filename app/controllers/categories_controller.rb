f0bdd38eddce44348d6741a1f9a95426fd4f4ef3
emerson000 <emerson@centercad.com>
2024-11-06 19:10:54 -0500

    add: turbo rails

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,6 +20,12 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path, notice: "Category was successfully deleted."
+  end
+
   def category_params
     params.require(:category).permit(:name)
   end
