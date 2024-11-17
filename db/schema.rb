0fed9072c52c027ab6d13d0c5a6b0b19bb785167
emerson000 <emerson@centercad.com>
2024-11-17 08:50:45 -0500

    update: rails 8.0.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_27_154750) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_17_134612) do
   create_table "answer_choices", force: :cascade do |t|
     t.string "text"
     t.boolean "is_correct"
